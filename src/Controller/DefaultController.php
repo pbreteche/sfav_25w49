534dcee5cccfd8a973f4ea25ea5f1f6d53a5b0b0
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-01 15:31:20 +0100

    Intro cache contracts

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use App\Event\MyCustomEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 #[Route('/')]
@@ -18,5 +21,25 @@ final class DefaultController extends AbstractController
         $eventDispatcher->dispatch($event, MyCustomEvent::NAME);
 
         return $this->render('default/index.html.twig');
+    }
+
+    #[Route('/cache')]
+    public function demoCache(
+        CacheInterface $cache,
+    ): Response {
+        $stopWatch = new Stopwatch();
+        $stopWatch->start('computation');
+        $data = $cache->get('demo_cache', function (ItemInterface $item) {
+            $item->expiresAfter(30);
+            sleep(2);
+
+            return 42;
+        });
+        $duration = $stopWatch->stop('computation')->getDuration();
+
+        return $this->render('default/cache.html.twig', [
+            'answer' => $data,
+            'duration' => $duration,
+        ]);
     }
 }
