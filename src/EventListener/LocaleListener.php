fba5954c2e71002929ae7d475e9fee06dd11ad21
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-01 12:37:36 +0100

    Proposition Atelier pratique

--- src/EventListener/LocaleListener.php
+++ src/EventListener/LocaleListener.php
@@ -21,6 +21,16 @@ final class LocaleListener
     #[AsEventListener(priority: 60)]
     public function getLocaleFromSession(RequestEvent $event): void
     {
+        $request = $event->getRequest();
+        if (
+            $request->query->has('_locale')
+            && in_array($request->query->get('_locale'), ['en', 'fr', 'es', 'de'])
+        ) {
+            $request->getSession()->set('_locale', $request->query->get('_locale'));
+        }
 
+        if ($request->getSession()->get('_locale')) {
+            $request->setLocale($request->getSession()->get('_locale'));
+        }
     }
 }
