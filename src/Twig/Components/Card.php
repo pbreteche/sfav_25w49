d071de8c1212f1f01b9d6521029c499b71f408e2
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-04 10:21:59 +0100

    UX Twig component : montage des props

--- src/Twig/Components/Card.php
+++ src/Twig/Components/Card.php
@@ -3,10 +3,20 @@
 namespace App\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
+use function Symfony\Component\String\u;
 
 #[AsTwigComponent]
 final class Card
 {
     public string $title;
     public ?string $image = null;
+
+    #[PreMount]
+    public function preMount(array $data): array
+    {
+        $data['title'] = u($data['title'])->title(true);
+
+        return $data;
+    }
 }
