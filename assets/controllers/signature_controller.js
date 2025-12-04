6b92bde5c603ea1786b28235bec26bf6fc677b72
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-04 14:56:46 +0100

    Stimulus, passer un paramètre (valeur)

--- assets/controllers/signature_controller.js
+++ assets/controllers/signature_controller.js
@@ -2,13 +2,22 @@ import {Controller} from "@hotwired/stimulus";
 import SignaturePad from "signature_pad";
 
 export default class extends Controller {
-    static targets = [ "canvas", "output"]
+    static targets = [ "canvas", "output", "submitButton"]
+    static values = {floor: Number}
     connect() {
         this.pad = new SignaturePad(this.canvasTarget);
+        this.submitButtonTarget.disabled = true;
+        this.pad.addEventListener('endStroke', () => this.validate())
     }
 
     clear() {
         this.pad.clear();
+        this.submitButtonTarget.disabled = true;
+    }
+
+    validate() {
+        const points = this.pad.toData().reduce((prev, cur) => prev + cur.points.length, 0);
+        this.submitButtonTarget.disabled = points < this.floorValue;
     }
 
     commit() {
