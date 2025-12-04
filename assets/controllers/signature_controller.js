f5177c4b1dd6bb4921e2121579a8754f4033bcd1
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-04 11:37:04 +0100

    Stimulus, ajout d'une action "effacer"

--- assets/controllers/signature_controller.js
+++ assets/controllers/signature_controller.js
@@ -4,7 +4,11 @@ import SignaturePad from "signature_pad";
 export default class extends Controller {
     static targets = [ "canvas" ]
     connect() {
-        const pad = new SignaturePad(this.canvasTarget);
+        this.pad = new SignaturePad(this.canvasTarget);
+    }
+
+    clear() {
+        this.pad.clear();
     }
 }
 
