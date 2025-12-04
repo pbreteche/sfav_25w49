73562b2c3812f158170abe3cd5770f57f6dcad1e
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-04 17:02:49 +0100

    Soumission du formulaire par le live component

--- src/Twig/Components/PostForm.php
+++ src/Twig/Components/PostForm.php
@@ -4,9 +4,12 @@ namespace App\Twig\Components;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
@@ -23,5 +26,22 @@ final class PostForm extends AbstractController
     protected function instantiateForm(): FormInterface
     {
         return $this->createForm(PostType::class, $this->initialData);
+    }
+
+    #[LiveAction]
+    public function save(
+        EntityManagerInterface $entityManager,
+    ): Response {
+        // Soumet et valide les données du formulaire
+        // Lève une exception UnprocessableEntityHttpException (422)
+        // L'exception est gérée par LiveController
+        $this->submitForm();
+
+        $entityManager->persist($this->getForm()->getData());
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Post saved!');
+
+        return $this->redirectToRoute('app_default_index');
     }
 }
