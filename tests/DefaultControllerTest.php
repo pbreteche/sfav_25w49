d70ca6301caa8f65d122fb3e7a2bc3112be1e74e
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-05 15:43:33 +0100

    Tester un formulaire

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -18,9 +18,23 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         // Validate a successful response and some content
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello world!');
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h1', 'Hello world!');
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("Hello world")')->count());
+        $client->followRedirects(false);
+
+        // La suite n'est pas fonctionnelle ici car nous avons utilisé un Live Component
+        // pour traiter les données du formulaire
+        // Le contrôleur ne tient pas compte des données transmises en POST
+        $form = $crawler->selectButton('Enregistrer')->form();
+        $input = ['post[body]' => 'body value'];
+        $client->submit($form, $input);
+        self::assertResponseIsUnprocessable();
+
+        $form = $crawler->selectButton('Enregistrer')->form();
+        $input['post[title]'] = 'title value';
+        $client->submit($form, $input);
+        self::assertResponseIsSuccessful();
     }
 }
