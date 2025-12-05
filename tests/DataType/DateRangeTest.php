13157404fc46bcf3f27314e613ab3c2d07502fbe
Pierre Bretéché <pbreteche@dawan.fr>
2025-12-05 11:16:29 +0100

    Plus d'assertions

--- tests/DataType/DateRangeTest.php
+++ tests/DataType/DateRangeTest.php
@@ -23,5 +23,24 @@ class DateRangeTest extends TestCase
         $this->assertInstanceOf(DateRange::class, $intersection);
         $this->assertEquals($dateRange2->getFrom(), $intersection->getFrom(), 'From date should be the later');
         $this->assertEquals($dateRange1->getTo(), $intersection->getTo(), 'To date should be the earlier');
+
+        $intersection = $dateRange2->intersect($dateRange1);
+
+        $this->assertInstanceOf(DateRange::class, $intersection);
+        $this->assertEquals($dateRange2->getFrom(), $intersection->getFrom(), 'From date should be the later');
+        $this->assertEquals($dateRange1->getTo(), $intersection->getTo(), 'To date should be the earlier');
+
+        $dateRange1 = new DateRange(
+            new \DateTimeImmutable('2025-12-01'),
+            new \DateTimeImmutable('2025-12-03'),
+        );
+        $dateRange2 = new DateRange(
+            new \DateTimeImmutable('2025-12-05'),
+            new \DateTimeImmutable('2025-12-07'),
+        );
+
+        $intersection = $dateRange1->intersect($dateRange2);
+
+        $this->assertNull($intersection);
     }
 }
